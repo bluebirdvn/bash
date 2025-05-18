3dcd1a035edcbddff6fa5ea167d3dd186c5ebb68
Blue Bird <shun.bluebird@gmail.com>
2025-05-18 21:33:34 +0700

    -z -n cmp string

--- cmpstring.sh
+++ cmpstring.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 
-read -p "String 1: " str1
-read -p "String 2: " str2
-
-if [ "$Str1" = "$Str2" ]
-then
-	echo "Strings are eq"
-else 
-	echo "String is not eq"
-fi
-if [ "$Str1" == "$Str2" ]
-then
-	echo "Strings are eq"
-else 
-	echo "String is not eq"
-fi
-if [[ "$Str1" != "$Str2" ]];then
-	echo "String is not ep"
+my="abc"
+if [[ -z "$my" ]];then
+	echo "string is not zero length"
+else
+	echo "string is zero length"
 fi
 
-
-string="Nowaday, linux is more popular to many people in the world"
-
-if [[ "$string" == *"linux"* ]];then
-	echo "linux is there"
+if [[ -n "$my" ]];then
+	echo "string is zero length"
+else
+	echo "string is not zero length"
 fi
 
 
