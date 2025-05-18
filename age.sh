6d3a4e1c45a57c91448c431f8d5704b5979a044b
Blue Bird <shun.bluebird@gmail.com>
2025-05-18 23:19:10 +0700

    menu config

--- age.sh
+++ age.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
-PS3="choose your country: "
 
-select country in germany france usa england
+PS3="your choice: "
+select item in "Add user" "List all processes" "Install program" "Quit"
 do
-	case $REPLY in
-	1)
-		echo "German"
-		;;
-	2)
-		echo "france"
-		;;
-	3)
-		echo "usa"
-		;;
-	4)
-		echo "england"
-		;;
-	5)
-		echo "quitting.."
+	if [[ $REPLY -eq 1 ]];then
+		read -p "Enter user nam" name
+		output="$(grep -w $name /etc/passwd)"
+		if [[ -n "$ouput" ]];then
+			echo "The username extsts"
+		else
+			sudo useradd -m -s /bin/bash "$username"
+			if [[ $? -eq 0 ]];then
+				echo "the user $name added success"
+				tail -n 1 /etc/passwd
+			else
+				echo "there was an error"
+			fi
+		fi
+
+	elif [[ $REPLY -eq 2 ]];then
+		echo "listing the processing "
+		sleep 1
+		ps -ef
+	elif [[ $REPLY -eq 3 ]];then
+		read -p "Enter the process kill: " process
+		pkill $process
+	elif [[ $REPLY -eq 4 ]];then
+		read -p "Enter the program to install: " app
+		sudo apt update && sudo apt install $app -y
+	elif [[ $REPLY -eq 5 ]];then
+		echo "Quitting..."
 		sleep 1
 		exit
-		;;
-	*)
-		echo "not valid"
-		;;
-		
-	esac
+	else
+		echo "invalid menu selection"
+	fi
 
 done
