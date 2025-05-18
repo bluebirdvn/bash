a983f824a19a24fff60ce9198bd5a2564deb25b7
Blue Bird <shun.bluebird@gmail.com>
2025-05-18 22:44:06 +0700

    do done

--- age.sh
+++ age.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
+PS3="choose your country: "
 
-echo -n "Enter your favorite pet: "
-read pet
-case "$pet" in
-"dog")
-	echo "your fa is dog"
-	;;
-"cat" | "Cat" | "kitty")
-	echo "you like cat"
-	;;
-fish)
-	echo "you like fish"
-	;;
-*)
-	echo "None"
-esac
+select country in germany france usa england
+do
+	case $REPLY in
+	1)
+		echo "German"
+		;;
+	2)
+		echo "france"
+		;;
+	3)
+		echo "usa"
+		;;
+	4)
+		echo "england"
+		;;
+	5)
+		echo "quitting.."
+		sleep 1
+		exit
+		;;
+	*)
+		echo "not valid"
+		;;
+		
+	esac
 
+done
