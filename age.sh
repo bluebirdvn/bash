585da6725b41db3836f521930ac5ea90a48d536a
Blue Bird <shun.bluebird@gmail.com>
2025-05-18 21:20:33 +0700

    multi ar, 0 -eq 1

--- age.sh
+++ age.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 read -p "Enter your age: " age
 
-if [[ $age -lt 18 && $age -ge 0 ]]
+if [[ $age -lt 18 -a $age -ge 0 ]]
 then 
 	echo "you are young"
 elif [[ $age -eq 18 ]]
 then 
 	echo "you are same me"
-elif [[ $age -gt 18 && $age -le 100 ]]
+elif [[ $age -gt 18 -a $age -le 100 ]]
 then	
 	echo "you are senior"
 else
