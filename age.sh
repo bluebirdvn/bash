5d6e6d165fb30c2802c729e8c35cdd5529fb3d56
Blue Bird <shun.bluebird@gmail.com>
2025-05-18 22:11:45 +0700

    case

--- age.sh
+++ age.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+echo -n "Enter your favorite pet: "
+read pet
+case "$pet" in
+"dog")
+	echo "your fa is dog"
+	;;
+"cat" | "Cat" | "kitty")
+	echo "you like cat"
+	;;
+fish)
+	echo "you like fish"
+	;;
+*)
+	echo "None"
+esac
 
-output="$(ping -c 3 $1)"
-#echo $output
-
-if [[ $output == *"100% packet loss"* ]];then
-	echo "the network connection to $1 is not working"
-else
-	echo "the connectio to $1 is working"
-fi
