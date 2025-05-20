dda208c14671ecde211a73ca4c062c345ef8fca1
Blue Bird <shun.bluebird@gmail.com>
2025-05-20 16:17:33 +0700

    update while

--- dropip.sh
+++ dropip.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-for ip in $(cat ips.txt);do
-	echo "Drop ip is $ip"
+#for ip in $(cat ips.txt);do
+#	echo "Drop ip is $ip"
+#	sudo iptables -I INPUT -s $ip -j DROP
+#done
+
+
+while read ip
+do
+	echo "Drop packets from ip $ip"
 	sudo iptables -I INPUT -s $ip -j DROP
-done
+	sleep 0.5
+done < ./ips.txt
